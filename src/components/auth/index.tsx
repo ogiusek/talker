d10961382ff5697f0de51aeac3373239252ad709
ogiusek <kowalewski.olgierd@gmail.com>
2023-07-17 00:11:10 +0200

    Declared the structure of Main components and prepared main sccc

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch, redirect } from "stupid-react-router";
 
+import Login from "./login";
+import Register from "./register";
+import Information from "./information";
+
 function Auth() {
   return (<Switch>
-    <Route path="/auth/login"></Route>
-    <Route path="/auth/register"></Route>
-    <Route path="/auth"></Route>
+    <Route path="/auth/login"><Login /></Route>
+    <Route path="/auth/register"><Register /></Route>
+    <Route path="/auth"><Information /></Route>
 
     <Route path="/">{redirect('/auth')}</Route>
   </Switch>);
