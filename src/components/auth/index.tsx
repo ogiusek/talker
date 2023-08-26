f101e4bad3108efe893b1449c9947c5e7fe6a49d
ogius <kowalewski.olgierd@gmail.com>
2023-08-27 00:47:16 +0200

    Finished first version on auth

--- src/components/auth/index.tsx
+++ src/components/auth/index.tsx
@@ -4,10 +4,12 @@ import { Route, Switch, Redirect } from "stupid-react-router";
 import Login from "./login";
 import Register from "./register";
 import Information from "./information";
+import { RegisterConfirm } from "./registerConfirm";
 
 function Auth() {
   return (<Switch>
     <Route path="/auth/login"><Login /></Route>
+    <Route path="/auth/register/confirm"><RegisterConfirm /></Route>
     <Route path="/auth/register"><Register /></Route>
     <Route path="/auth"><Information /></Route>
 
